08c82d1e8e68daabada3f508aae0a48650269b34
Heinz <araqueheinz@gmail.com>
2018-04-19 17:30:10 -0400

    Radio buttons not working

---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.getElementById('blue').addEventListener('click',function(){
+    document.getElementById('phone-case').src = 'images/Iphone6_Blue.png'
+    document.getElementById('phone-case').alt = 'blue phone case picture'
+})
+
+document.getElementById('red').addEventListener('click',function(){
+    document.getElementById('phone-case').src = 'images/Iphone6_Red.png'
+    document.getElementById('phone-case').alt = 'Red phone case picture'
+})
+
+document.getElementById('green').addEventListener('click',function(){
+    document.getElementById('phone-case').src = 'images/Iphone6_Green.png'
+    document.getElementById('phone-case').alt = 'Green phone case picture'
+})
+
+document.getElementById('pink').addEventListener('click',function(){
+    document.getElementById('phone-case').src = 'images/Iphone6_Pink.png'
+    document.getElementById('phone-case').alt = 'ink phone case picture'
+})
