6125f62a68f428a7bc6afd0e0d8b0428dde91300
Heinz Araque <araqueheinz@gmail.com>
2018-04-20 17:16:28 -0400

    Fix the radio buttons

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 document.getElementById('blue').addEventListener('click',function(){
-    document.getElementById('phone-case').src = 'images/Iphone6_Blue.png'
-    document.getElementById('phone-case').alt = 'blue phone case picture'
+    document.getElementById('phone-case').src = 'images/Iphone6_Blue.png';
+    document.getElementById('phone-case').alt = 'blue phone case picture';
+    document.getElementById('price').innerHTML = '8 UltraMillion';
+  
 })
 
 document.getElementById('red').addEventListener('click',function(){
-    document.getElementById('phone-case').src = 'images/Iphone6_Red.png'
-    document.getElementById('phone-case').alt = 'Red phone case picture'
+    document.getElementById('phone-case').src = 'images/Iphone6_Red.png';
+    document.getElementById('phone-case').alt = 'Red phone case picture';
+    document.getElementById('price').innerHTML = '7 MultiMillion';
 })
 
 document.getElementById('green').addEventListener('click',function(){
-    document.getElementById('phone-case').src = 'images/Iphone6_Green.png'
-    document.getElementById('phone-case').alt = 'Green phone case picture'
+    document.getElementById('phone-case').src = 'images/Iphone6_Green.png';
+    document.getElementById('phone-case').alt = 'Green phone case picture';
+    document.getElementById('price').innerHTML = '2 Chickens'
 })
 
 document.getElementById('pink').addEventListener('click',function(){
     document.getElementById('phone-case').src = 'images/Iphone6_Pink.png'
     document.getElementById('phone-case').alt = 'ink phone case picture'
+    document.getElementById('price').innerHTML = '22 Billion'
 })
