17a4c2f544e28b12b16b197288c525167ff186fb
Heinz <araqueheinz@gmail.com>
2018-04-23 13:51:06 -0400

    zip code and email verification are working

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ document.getElementById('green').addEventListener('click',function(){
 
 document.getElementById('pink').addEventListener('click',function(){
     document.getElementById('phone-case').src = 'images/Iphone6_Pink.png'
-    document.getElementById('phone-case').alt = 'ink phone case picture'
+    document.getElementById('phone-case').alt = 'Pink phone case picture'
     document.getElementById('price').innerHTML = '$16.00';
     document.getElementById('selected-product').innerHTML = 'Case color: Pink';
 })
@@ -37,6 +37,7 @@ document.getElementById('pink').addEventListener('click',function(){
 /* /////////////////////////////////////////////////////////////////////// */
 /* /                          IPHONE TYPE CONTROLS                       / */
 /* /////////////////////////////////////////////////////////////////////// */
+
 
 document.getElementById('iphone-6').addEventListener('click',function(){
     document.getElementById('selected-phone').innerHTML = 'For: iPhone 6';
@@ -49,3 +50,63 @@ document.getElementById('iphone-7').addEventListener('click',function(){
 document.getElementById('iphone-8').addEventListener('click',function(){
     document.getElementById('selected-phone').innerHTML = 'For: iPhone 8';
 })
+
+
+/* /////////////////////////////////////////////////////////////////////// */
+/* /                          VERIFICATION CONTROLS                      / */
+/* /////////////////////////////////////////////////////////////////////// */
+
+
+let allowSubmit = false;
+var userAddress = document.getElementById('address').value;
+var userCity = document.getElementById('city').value;
+var userState = document.getElementById('state').value;
+
+var userCountry = document.getElementById('country').value;
+
+document.getElementById('name').addEventListener('blur', function(event){
+   
+    var userName = document.getElementById('name').value;
+    document.getElementById('name-address').innerHTML = userName;;
+})
+
+document.getElementById('email').addEventListener('blur', function(event){
+
+    const userEmail = document.getElementById('email').value;
+    const patt = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
+
+    const validation = patt.test(userEmail);
+    if(validation){
+        document.querySelector('label[for=email]').classList.toggle('wrong-input');
+
+    }
+    else{
+        console.log(validation);
+        document.querySelector('label[for=email]').classList.add('wrong-input');
+    }
+
+
+
+})
+document.getElementById('city').addEventListener('blur', function(event){
+    
+})
+document.getElementById('state').addEventListener('blur', function(event){
+    
+})
+document.getElementById('zip').addEventListener('blur', function(event){
+
+    var userZip = document.getElementById('zip').value;
+    const patt = /^\d{5}(\-?\d{4})?$/gm;
+
+    const validation = patt.test(userZip);
+    if(validation){
+        document.querySelector('label[for=zip]').classList.toggle('wrong-input');
+    }
+    else{
+        document.querySelector('label[for=zip]').classList.add('wrong-input');
+    }
+
+    
+})
+ 
