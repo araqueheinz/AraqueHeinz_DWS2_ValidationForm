91c60ddee2eff6cc49f630736c36c2395a4d99bd
Heinz <araqueheinz@gmail.com>
2018-04-22 23:12:35 -0400

    trying to solve and issue with the positioning of inputs and labels in the form

--- js/main.js
+++ js/main.js
@@ -1,24 +1,51 @@
+
+/* /////////////////////////////////////////////////////////////////////// */
+/* /                          COLOR BUTTONS CONTROLS                     / */
+/* /////////////////////////////////////////////////////////////////////// */
+
+
 document.getElementById('blue').addEventListener('click',function(){
     document.getElementById('phone-case').src = 'images/Iphone6_Blue.png';
     document.getElementById('phone-case').alt = 'blue phone case picture';
-    document.getElementById('price').innerHTML = '8 UltraMillion';
+    document.getElementById('price').innerHTML = '$13.00';
+    document.getElementById('selected-product').innerHTML = 'Case color: Blue';
   
 })
 
 document.getElementById('red').addEventListener('click',function(){
     document.getElementById('phone-case').src = 'images/Iphone6_Red.png';
     document.getElementById('phone-case').alt = 'Red phone case picture';
-    document.getElementById('price').innerHTML = '7 MultiMillion';
+    document.getElementById('price').innerHTML = '$15.00';
+    document.getElementById('selected-product').innerHTML = 'Case color: Red';
 })
 
 document.getElementById('green').addEventListener('click',function(){
     document.getElementById('phone-case').src = 'images/Iphone6_Green.png';
     document.getElementById('phone-case').alt = 'Green phone case picture';
-    document.getElementById('price').innerHTML = '2 Chickens'
+    document.getElementById('price').innerHTML = '$11.00';
+    document.getElementById('selected-product').innerHTML = 'Case color: Green';
 })
 
 document.getElementById('pink').addEventListener('click',function(){
     document.getElementById('phone-case').src = 'images/Iphone6_Pink.png'
     document.getElementById('phone-case').alt = 'ink phone case picture'
-    document.getElementById('price').innerHTML = '22 Billion'
+    document.getElementById('price').innerHTML = '$16.00';
+    document.getElementById('selected-product').innerHTML = 'Case color: Pink';
+})
+
+
+/* /////////////////////////////////////////////////////////////////////// */
+/* /                          IPHONE TYPE CONTROLS                       / */
+/* /////////////////////////////////////////////////////////////////////// */
+
+document.getElementById('iphone-6').addEventListener('click',function(){
+    document.getElementById('selected-phone').innerHTML = 'For: iPhone 6';
+})
+
+document.getElementById('iphone-7').addEventListener('click',function(){
+    document.getElementById('selected-phone').innerHTML = 'For: iPhone 7';
+})
+
+document.getElementById('iphone-8').addEventListener('click',function(){
+    document.getElementById('selected-phone').innerHTML = 'For: iPhone 8';
 })
--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@ Project: Accessibility and Advanced Form Validation
 For this project I will build a responsive, accessible shopping/checkout form with custom validation. 
 
 Website link for the website:
-
+https://araqueheinz.github.io/AraqueHeinz_DWS2_ValidationForm/
